5098564a293b61b34fe264022ed527dcaa51f3c0
MSI\moneo <jvr.moneo@gmail.com>
2020-11-23 12:40:14 +0100

    Crear estructura del proyecto

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import { usersController } from '../controllers/users.controller';
+
+class UsersRoutes {
+
+    public router: Router = Router();
+
+    constructor(){
+        this.router.get('/show', usersController.show);
+        
+        this.router.get('/', usersController.index);
+        this.router.post('/', usersController.create);
+        this.router.delete('/:id', usersController.delete);
+        this.router.put('/:id', usersController.update);
+
+        this.router.get('/providers', usersController.providers);
+    }
+}
+
+export const userRoutes = new UsersRoutes();
